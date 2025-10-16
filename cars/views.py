e7320dfee0620580f1986ea11a718e30a92ecf59
Lucas Henrique <lucashc789@gmail.com>
2025-10-16 17:46:59 -0300

    Projeto django

---
+++ cars/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from cars.models import Car
+from cars.forms import CarModelForm
+
+
+def cars_view(request):
+
+    search = request.GET.get('search')   
+    cars = Car.objects.all()
+
+    if search:
+        cars = Car.objects.filter(model__icontains = search).order_by('model')
+    
+    print('Lista de objetos:', cars)
+
+    print("Requisição:", request.GET.get('search'))
+    
+    return render(
+                request,  
+                'cars.html',
+                 {'cars': cars } 
+                 
+    )
+
+def new_car_view(request):
+    if request.method == 'POST':
+        new_car_form = CarModelForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+    else:
+        new_car_form = CarModelForm()
+        
+    return render(
+        request,
+        'new_car.html',
+        {'new_car_form': new_car_form}
+    )
+    
